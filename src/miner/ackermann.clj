d5aefc48dffea9aa9f85b2ba8915dc7d8b54122f
Steve Miner <steveminer@gmail.com>
2015-11-14 13:45:11 -0500

    fixes for ack

--- src/miner/ackermann.clj
+++ src/miner/ackermann.clj
@@ -19,6 +19,28 @@
 ;; (ack 3 11) works but (ack 3 12) StackOverflowError
 
 
+;; trampoline-style, but pretty slow (slower than ack), and still overflows
+(defn ackt [m n]
+  (cond (fn? n) (recur m (trampoline n))
+        (zero? m) (inc n)
+        (zero? n) (recur (dec m) 1)
+        :else (recur (dec m) #(ackt m (dec n)))))
+
+
+;; bad
+;; (defn ack4
+;;   ([m n] (ack4 0 m m n))
+;;   ([c m2 m n]
+;;    (if (zero? c)
+;;      (cond (zero? m) (inc n)
+;;            (zero? n) (recur 0 m (dec m) 1)
+;;            :else (recur 0 m (dec m) (ack4 0 m m (dec n))))
+;;      (recur (dec c) (dec m) (dec m) n
+;; 
+
+
+
+
 ;; Much faster!
 ;; derived from CL version, better algorithm but not obviously equivalent
 ;; and subject to overflow for m > 3
@@ -43,11 +65,29 @@
 
 
 
+(defn ack-3a
+"The Ackermann function using a stack and only tail recursion.
+  Thanks to Allan Malloy's post at:
+  http://grokbase.com/p/gg/clojure/127rbk4518/reduction-to-tail-recursion"
+  [m n]
+  (loop [^long m m ^long n n stack ()]
+    (when-not (empty? stack)
+      (when-let [sss (seq (filter #(> ^long % 2) stack))]
+      (println (count stack) sss)))
+    (cond
+      (zero? m) (if (empty? stack)
+                  (inc n)
+                  (recur (peek stack) (inc n) (pop stack)))
+      (zero? n) (recur (dec m) 1 stack)
+      :else (recur m (dec n) (conj stack (dec m))))))
+
+
+
 ;; (ack2a 3 50)
 ;; 9007199254740989
 
 ;; https://github.com/bluemont/ackermann
-;; works but very slow compared to ack2a
+;; works but very slow compared to ack2a but can handle m=4
 (defn ack-3
 "The Ackermann function using a stack and only tail recursion.
   Thanks to Allan Malloy's post at:
@@ -78,7 +118,7 @@
     (/ (rprod (- n k -1) n) (rprod 1 k))))
 
 (defn coeffs [k]
-  (map (partial binomial-coefficients k) (range (inc k))))
+  (map (partial binomial-coefficient k) (range (inc k))))
 
 (defn bico [n k]
   (let [rprod (fn [a b] (reduce * 1 (range a (inc b))))]
@@ -86,3 +126,5 @@
 
 (defn bicos [k]
   (map (partial bico k) (range (inc k))))
+
+
