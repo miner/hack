6ad2541928280a0d2aa021813ca82368db915649
Steve Miner <steveminer@gmail.com>
2016-06-17 16:35:12 -0400

    lots of updates

--- project.clj
+++ project.clj
@@ -1,37 +1,41 @@
-(defproject hack "0.2.7-SNAPSHOT" 
+(defproject hack "0.2.8-SNAPSHOT" 
   :min-lein-version "2.0.0"
   :dependencies [
                  [racehub/om-bootstrap "0.6.1" :exclusions [org.clojure/clojure]]
                  [com.velisco/wilkins "0.2.1"]
-                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/core.async "0.2.385"]
                  [org.clojure/tools.macro "0.1.5"]
-                 [com.datomic/datomic-free "0.9.5359"  :exclusions [joda-time]]
-                 [instaparse "1.4.1"]
+                 [com.datomic/datomic-free "0.9.5372"  :exclusions [joda-time]]
+                 [instaparse "1.4.2"]
                  ;;[clj-webdriver "0.5.1"]
                  [org.clojure/core.logic "0.8.10"]
                  [quil "2.4.0"]
                  ;; [com.velisco/herbert "0.6.1"]
-                  [com.velisco/herbert "0.6.11"]
+                  [com.velisco/herbert "0.7.0"]
                  [org.clojure/algo.monads "0.1.5"]
                  [org.clojure/tools.trace "0.7.9"]
                  ;; [org.clojure/core.match "0.2.2"]
                  [org.clojure/core.match "0.3.0-alpha3"]
                  [com.velisco/halfbaked "0.2.3"]
-                 [com.velisco/clj-ftp "0.3.5"]
-                 [metrics-clojure "2.6.1"]
+                 [com.velisco/clj-ftp "0.3.6"]
+                 [metrics-clojure "2.7.0"]
                  [digest "1.4.4"]
                  ;; [it.unimi.dsi/sux4j "3.0.10"] ;perfect-hashing
                  [me.raynes/fs "1.4.6"]]
   :profiles {:dev  {:dependencies [   
-                                   [org.clojure/clojure "1.8.0"]
-                                   [org.clojure/core.async "0.2.374"]
+                                   [org.clojure/clojure "1.9.0-alpha7"]
+                                   [org.clojure/core.async "0.2.385"]
                                    ;;[clj-ns-browser "1.3.0" ]
                                    [criterium "0.4.4"]]}
+             :clj18 {:dependencies [   
+                                   [org.clojure/clojure "1.7.0"]
+                                   ]}
+
              :clj17 {:dependencies [   
                                    [org.clojure/clojure "1.7.0"]
                                    ]}
 
-             :snapshot {:dependencies [[org.clojure/clojure "1.8.0-master-snapshot"]]}
+             :snapshot {:dependencies [[org.clojure/clojure "1.9.0-master-snapshot"]]}
              }
   :repositories [["sonatype-oss-public" {:url "https://oss.sonatype.org/content/groups/public/"}]]
   ;; :bootclasspath true
