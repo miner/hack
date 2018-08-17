4908706924bbb1cabce7e3285068ef68302810df
Steve Miner <steveminer@gmail.com>
2018-08-17 13:04:59 -0400

    bitcount

--- src/miner/bitcount.clj
+++ src/miner/bitcount.clj
@@ -24,6 +24,30 @@
       (recur (bit-and v (dec v)) (inc c)))))
 
 
+(defn bkpos2
+  ([^long n] (if (neg? n)
+               (bkpos2 (bit-clear n 63) 1)
+               (bkpos2 n 0)))
+  ([^long n ^long c]
+   (loop [v n c c]
+    (if (zero? v)
+      c
+      (recur (bit-and v (dec v)) (inc c))))))
+
+
+;;; SEM -- interestingly, it's faster to use explicit loop than to recur to the function.
+;;; Is that because primitives work better?
+(defn bkpos3 [^long n]
+  (let [bkp (fn [^long v ^long c]
+              (loop [v v c c]
+               (if (zero? v)
+                 c
+                 (recur (bit-and v (dec v)) (inc c)))))]
+    (if (neg? n)
+      (bkp (bit-clear n 63) 1)
+      (bkp n 0))))
+
+
 ;; save this over hinted version
 ;; SEM: not sure all the casting is necessary
 (defn bkpos-works [^long n]
@@ -38,8 +62,42 @@
       
 
 
+(defn bytebcnt ^long [^long n]
+  (case (bit-and 0xFF n)
+    0 0,
+
+    (127 191 223 239 247 251 253 254) 7,
+
+    (1 2 4 8 16 32 64 128) 1,
+
+    (15 23 27 29 30 39 43 45 46 51 53 54 57 58 60 71 75 77 78 83 85 86 89 90 92 99 101 102
+    105 106 108 113 114 116 120 135 139 141 142 147 149 150 153 154 156 163 165 166 169 170
+    172 177 178 180 184 195 197 198 201 202 204 209 210 212 216 225 226 228 232 240) 4,
+
+    (63 95 111 119 123 125 126 159 175 183 187 189 190 207 215 219 221 222 231 235 237 238
+    243 245 246 249 250 252) 6,
+
+    (7 11 13 14 19 21 22 25 26 28 35 37 38 41 42 44 49 50 52 56 67 69 70 73 74 76 81
+    82 84 88 97 98 100 104 112 131 133 134 137 138 140 145 146 148 152 161 162 164 168 176
+    193 194 196 200 208 224) 3,
+
+    (3 5 6 9 10 12 17 18 20 24 33 34 36 40 48 65 66 68 72 80 96 129 130 132 136 144 160 192) 2,
+
+    (31 47 55 59 61 62 79 87 91 93 94 103 107 109 110 115 117 118 121 122 124 143 151 155
+    157 158 167 171 173 174 179 181 182 185 186 188 199 203 205 206 211 213 214 217 218 220
+    227 229 230 233 234 236 241 242 244 248) 5,
+
+    255 8))
+
+(defn bbcnt [^long n]
+  (loop [c 0 n n]
+    (if (zero? n)
+      c
+      (recur (+ c (bytebcnt n)) (unsigned-bit-shift-right n 8)))))
+    
 (defn test-bitcount [f]
-  (every? #(= (Integer/bitCount %) (f %)) (range 10000)))
+  (assert (every? #(= (Integer/bitCount %) (f %)) (range 10000)))
+  true)
 
 
 
