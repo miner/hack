e5a0663f64af26aee772fbe297cfaf8347ad6a21
Steve Miner <steveminer@gmail.com>
2019-05-25 09:29:46 -0400

    refactor

--- src/miner/lynchbell.clj
+++ src/miner/lynchbell.clj
@@ -7,14 +7,14 @@
 ;; digits have to be all different. ... Implement a search for the largest Lynch-Bell number."
 
 
-;; Independent discussion on Stack Exchange:
+;; Good discussion on Stack Exchange:
 ;; https://math.stackexchange.com/questions/2766109/biggest-lynch-bell-number#2766151
 ;;
 ;; My paraphrase:  Zero is excluded by definition.  Similarly, you can't have both 2 and
-;; 5 (2x5=10).  So you know there are no 10 or 9 digit LBs.  Any even digit and 5 would also
-;; be a problem producing a factor of 10 and an ending 0 digit.  So 5 is out.  Leaving
+;; 5 (2x5=10).  So there are no 10 or 9 digit LBs.  A 5 and any even digit would also
+;; be a problem, producing a factor of 10 and an ending 0 digit.  So 5 is out.  Leaving
 ;; 98764321 possible in some order.  But the sum of digits is 40 which means it can't be
-;; multiple of 9 (or 3).  So no eight digit LBs.
+;; multiple of 9.  So no eight digit LBs.
 ;;
 ;; We want to start with a 9 if possible.  That also means the LB is a multiple of 9.  So
 ;; the sum of the digits must be a multiple of 9.  The failing eight-digit sum was 40.  We
@@ -27,6 +27,12 @@
 ;; treatment.
 
 
+(defn digits [n]
+  (map #(- (long %) (long \0)) (str n)))
+
+(defn digs->long [digs]
+  (reduce #(+ (* 10 ^long %) ^long %2) 0 digs))
+
 (defn zmod? [^long n ^long d]
   (case d
     1 true
@@ -34,20 +40,14 @@
     (3 4 5 6 7 8 9) (zero? (rem n d))
     false))
 
-(defn digits [n]
-  (map #(- (long %) (long \0)) (str n)))
-
-(defn digs->long [digs]
-  (reduce #(+ (* 10 ^long %) ^long %2) 0 digs))
-
-
 (defn lb-digits? [n digs]
   (and (apply distinct? digs)
        (every? #(zmod? n %) digs)))
 
-;; returns the corresponding Lynch-Bell number or nil for failure.  Convenient for
-;; filtering/keeping.
-(defn lbn [digs]
+(defn lbn 
+  "Given a collection of digits, returns the corresponding Lynch-Bell number or nil for
+  failure."
+  [digs]
   (let [n (digs->long digs)]
     (when (lb-digits? n digs)
       n)))
@@ -60,7 +60,6 @@
 (defn search-max-lb [digs]
   (first (keep lbn (mc/permutations digs))))
 
-
 ;; returns collection of digits in high to low orders [9...1] excluding args
 (defn seed-excluding [& excluding]
   (remove (set excluding) (range 9 0 -1)))
@@ -68,9 +67,8 @@
 (defn find-largest-lb [n]
   ;; starting at N digits, should be [1..7]
   (first (keep search-max-lb (map #(apply seed-excluding %)
-                              (mapcat #(mc/combinations (range 1 10) %)
-                                      (range (- 9 n) 9))))))
-
+                                  (mapcat #(mc/combinations (range 1 10) %)
+                                          (range (- 9 n) 9))))))
 
 ;; From the previous discussion, we conclude that the search must start with seven digits.
 ;; The `seed-excluding` function returns a "seed" collection of digits with the given digits
@@ -85,7 +83,6 @@
       (find-largest-lb 7)))
 
 
-
 ;; Trying to keep the 9, we note that excluding the 4 gives us seven digits 9876321, whose
 ;; sum is 36 (a multiple of 9).  We use that as an initial seed and find success quickly.
 ;; I don't provide a proof that this short-cut is 100% correct, but it has the virtue of
@@ -96,34 +93,17 @@
   (search-max-lb [9 8 7 6 3 2 1]))
 
 
-
-;; bonus -- return all the Lynch-Bell numbers in order
-;; transducers for a slight performance boost
-(defn find-all-lynch-bell-numbers []
+;; bonus
+(defn find-all-lynch-bell-numbers 
+  "Returns a sequence of all Lynch-Bell numbers in ascending order"
+  []
   (sort (sequence (comp (mapcat #(mc/combinations (range 1 10) %))
                         (mapcat mc/permutations)
                         (keep lbn))
                   (range 1 8))))
 
-
-
-#_
-(time (find-largest-lb 8))
-;; "Elapsed time: 1313.236453 msecs" 9867312
-
-#_
-(time (find-largest-lb 7))
-;; "Elapsed time: 311.626788 msecs" 9867312
-
-#_
-(time (find-largest-lynch-bell))
-;; "Elapsed time: 43.613458 msecs" 9867312
-
-#_
-(time (fast-largest-lynch-bell))
-;; "Elapsed time: 0.313049 msecs" 9867312
-
-
+(defn slow-last-lynch-bell []
+  (last (find-all-lynch-bell-numbers)))
 
 
 ;; https://oeis.org/A115569
@@ -180,20 +160,37 @@
 (defn lb-test []
   (assert (every? lynch-bell? lb548))
   (assert (= (find-largest-lb 8) largest-lynch-bell))
+  (assert (= (slow-last-lynch-bell) largest-lynch-bell))
   (assert (= (find-largest-lynch-bell) largest-lynch-bell))
   (assert (= (fast-largest-lynch-bell) largest-lynch-bell))
   (assert (= (find-all-lynch-bell-numbers) lb548))
   
-  (println "Find largest Lynch-Bell starting at 8 digits")
+  (println "Find largest Lynch-Bell starting at 8 digits (safe but slow)")
   (time (find-largest-lb 8))
   (println)
-  (println "Find largest Lynch-Bell number")
+  (println "Last of all Lynch-Bell numbers, 7 digits")
+  (time (slow-last-lynch-bell))
+  (println)
+  (println "Find largest Lynch-Bell number, 7 digits, excluding 5")
   (time (find-largest-lynch-bell))
   (println)
-  (println "Fast")
+  (println "Fast, using math to limit search")
   (time (fast-largest-lynch-bell)))
 
 
 
-
-
+;; Tested on MacBook...
+;; (lb-test)
+;;
+;; Find largest Lynch-Bell starting at 8 digits (safe but slow)
+;; "Elapsed time: 1621.554474 msecs"
+;; 
+;; Last of all Lynch-Bell numbers, 7 digits
+;; "Elapsed time: 639.29202 msecs"
+;; 
+;; Find largest Lynch-Bell number, 7 digits, excluding 5
+;; "Elapsed time: 51.224306 msecs"
+;; 
+;; Fast, using math to limit search
+;; "Elapsed time: 0.096333 msecs"
+;; 9867312
