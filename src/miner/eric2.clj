de51c5958f780d564a38830e48591f42cd6a0a60
Steve Miner <steveminer@gmail.com>
2019-11-20 09:34:16 -0500

    more transducers

--- src/miner/eric2.clj
+++ src/miner/eric2.clj
@@ -33,25 +33,22 @@
 (defn neg-prev [prev x]
   (conj prev (- x)))
 
-(defn gen-candidates [coll]
+(defn eval-candidate [xs]
+  (reduce + 0 xs))
+
+(defn solutions []
   (let [negs #(expand-nums neg-prev %)
-        nums #(expand-nums combine-prev %)]
-    (into () (mapcat negs) (nums coll))))
-
-(defn eval-candidate [expr]
-  (reduce + 0 expr))
-
-(defn print-expr [expr]
-  (print (first expr))
-  (doseq [x (rest expr)]
-    (if (neg? x)
-      (print " -" (- x))
-      (print " +" x))))
-
+        nums #(expand-nums combine-prev %)
+        hundred? #(= (eval-candidate %) 100)]
+    (into () (comp (mapcat negs) (filter hundred?)) (nums (range 1 10)))))
+  
 (defn print-solutions []
-  (doseq [sol (filter (fn [xs] (= (eval-candidate xs) 100))
-                      (gen-candidates (range 1 10)))]
-    (print-expr sol)
+  (doseq [xs (solutions)]
+    (print (first xs))
+    (doseq [x (rest xs)]
+      (if (neg? x)
+        (print " -" (- x))
+        (print " +" x)))
     (println " = 100")))
 
 
@@ -62,191 +59,3 @@
 ;; Maybe a transducer? With mapcat? Something with tree-seq?
 
 
-
-;;; OLD STUFF
-
-
-
-
-
-(defn gen-candidates3 [coll]
-  (let [negs #(expand-nums neg-prev %)
-        nums #(expand-nums combine-prev %)]
-    (mapcat negs (nums coll))))
-
-(defn gen-exprs1 [coll]
-  (mapcat gen-negs (gen-nums coll)))
-
-(defn gen-negs [coll]
-  (letfn [(gen [acc prev coll]
-            (if (seq coll)
-              (-> acc
-                  (gen (conj prev (first coll)) (rest coll))
-                  (gen (conj prev (- (first coll))) (rest coll)))
-              (conj acc prev)))]
-    (when (seq coll)
-      (gen () [(first coll)] (rest coll)))))
-    
-(defn gen-nums [coll]
-  (letfn [(gen [acc prev coll]
-            (if (seq coll)
-              (-> acc
-                  (gen (conj prev (first coll)) (rest coll))
-                  (gen (conj (pop prev) (+ (first coll) (* 10 (peek prev)))) (rest coll)))
-              (conj acc prev)))]
-    (when (seq coll)
-      (gen () [(first coll)] (rest coll)))))
-
-
-
-(defn addsubs
-  ([coll] (addsubs [(first coll)] (rest coll)))
-  ([prev coll]
-   (if (seq coll)
-     (concat (addsubs (conj prev (first coll)) (rest coll))
-             (addsubs (conj prev (- (first coll))) (rest coll)))
-     (list prev))))
-
-
-;; rewrite with acc like keypaths
-(defn ncats
-  ([coll] (when (seq coll) (ncats [(first coll)] (rest coll))))
-  ([prev coll]
-   (if (seq coll)
-     (concat (ncats (conj prev (first coll)) (rest coll))
-             (ncats (conj (pop prev) (+ (first coll) (* 10 (peek prev)))) (rest coll)))
-     (list prev))))
-
-
-
-(defn gen-cats
-  ([coll] (when (seq coll) (gen-cats [] [(first coll)] (rest coll))))
-  ([acc prev coll]
-   (if (seq coll)
-     (-> acc
-         (gen-cats (conj prev (first coll)) (rest coll))
-         (gen-cats (conj (pop prev) (+ (first coll) (* 10 (peek prev)))) (rest coll)))
-     (conj acc prev))))
-
-
-(defn gen-cs1
-  ([coll] (when (seq coll) (persistent! (gen-cs1 (transient []) [(first coll)] (rest coll)))))
-  ([acc prev coll]
-   (if (seq coll)
-     (-> acc
-         (gen-cs1 (conj prev (first coll)) (rest coll))
-         (gen-cs1 (conj (pop prev) (+ (first coll) (* 10 (peek prev)))) (rest coll)))
-     (conj! acc prev))))
-
-
-
-;; almost good enough but still pretty slow
-(defn ncp [coll]
-  (let [vv (vec coll)]
-    (into []
-          (comp
-           (map (fn [ss]
-                  (reduce (fn [v i]
-                     (assoc (update v i + (* 10 (v (dec i))))
-                            (dec i) 0))
-                   vv
-                   ss)))
-                (map #(remove zero? %)))
-         (mc/subsets (range 1 (count vv))))))
-
-(defn print-sols []
-  (doseq [sol (filter (fn [xs] (= (apply + xs) 100))
-                           (mapcat addsubs (ncp (range 1 10))))]
-    (print (first sol))
-    (doseq [x (rest sol)]
-      (if (neg? x)
-        (print " -" (- x))
-        (print " +" x)))
-    (println " = 100")))
-
-
-
-
-
-
-(defn ncp1 [coll]
-  (let [vv (vec coll)]
-    (map (fn [ss]
-           (reduce (fn [v i]
-                     (assoc (update v i + (* 10 (v (dec i))))
-                            (dec i) nil))
-                   vv
-                   ss))
-         (mc/subsets (range 1 (count vv))))))
-
-(defn ncp2 [coll]
-  (let [vv (vec coll)]
-    (map #(remove zero? %)
-         (map (fn [ss]
-                (reduce (fn [v i]
-                          (assoc (update v i + (* 10 (v (dec i))))
-                                 (dec i) 0))
-                        vv
-                        ss))
-              (mc/subsets (range 1 (count vv)))))))
-         
-
-
-;; mc/partitions returns too many results for our purposes
-;; we only want ordered partitions.
-;; in a sense mc/subsets could tell us where to join by index
-
-;; very slow
-#_
-(defn ncs [coll]
-  (map (fn [xvs] (mapv #(reduce (fn [r x] (+ x (* 10 r))) 0 %) xvs))
-       (filter (fn [parts]
-                 (every? #(reduce (fn [a b] (if (= (inc a) b) b (reduced false))) %)
-                         parts))
-               (mc/partitions coll))))
-
-
-
-(defn n100 []
-  (filter (fn [xs] (= (apply + xs) 100))
-          (mapcat addsubs (ncats (range 1 10)))))
-
-
-
-
-
-
-;; not needed below
-(comment
-
-(defn oneton [n]
-  (mapcat addsubs (ncats (range 1 (inc n)))))
-
-(defn pmc
-  ([coll] (pmc [(first coll)] (rest coll)))
-  ([prev coll]
-   (if (seq coll)
-     (mapcat (fn [op] (pmc (conj prev op (first coll)) (rest coll))) '[+ -])
-     [prev])))
-
-
-
-;; Simply with pos/neg numbers -- never gen operators! Never eval exprs!
-
-(defn pms [colls]
-  (mapcat pmc colls))
-
-(defn onen [n]
-  (pms (ncats (range 1 (inc n)))))
-
-(defn evalexpr [expr]
-  (let [[a op b & more] expr]
-    (loop [a a op op b b more more]
-      (cond (nil? op) a
-            (= op '+) (recur (+ a b) (first more) (second more) (nnext more))
-            (= op '-) (recur (- a b) (first more) (second more) (nnext more))
-            :else (throw (ex-info "Bad expr" {:expr expr :more more :a a :op op :b b}))))))
-
-
-
-)
