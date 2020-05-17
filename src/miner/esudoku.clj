0d83bcdaec9bf81c17a0b078a4b7a5aafba05aad
Steve Miner <steveminer@gmail.com>
2020-05-16 20:49:53 -0400

    faster bits

--- src/miner/esudoku.clj
+++ src/miner/esudoku.clj
@@ -55,8 +55,51 @@
                               (mapcat #(subvec % (* c 3) (* (inc c) 3))
                                       (subvec board (* r 3) (* (inc r) 3))))))))
 
+
+(defn bs? [board]
+  (let [valid-row? (fn [row] (zero? (reduce bit-flip 1022 row)))]
+    (and (every? valid-row? board)
+         (reduce (fn [res c]
+                   (if (zero? (reduce (fn [col r]
+                                 (bit-flip col (nth (nth board r) c)))
+                               1022
+                               (range 9)))
+                     true
+                     (reduced false)))
+                 true
+                 (range 9))
+         (every? valid-row? (for [r (range 3)  c (range 3)]
+                              (mapcat #(subvec % (* c 3) (* (inc c) 3))
+                                      (subvec board (* r 3) (* (inc r) 3))))))))
+
+
+
+
+(defn bs1? [board]
+  (let [valid-row? (fn [row] (zero? (reduce bit-flip 1022 row)))]
+    (and (every? valid-row? board)
+         (reduce (fn [res c]
+                   (if (zero? (reduce (fn [col row]
+                                 (bit-flip col (nth row c)))
+                               1022
+                               board))
+                     true
+                     (reduced false)))
+                 true
+                 (range 9))
+
+        (every? valid-row?  (partition 9 (for [bc (range 3)
+               br (range 3)
+               i (range (* bc 3) (* (inc bc) 3))
+               j (range (* br 3) (* (inc br) 3))]
+           (nth (nth board j) i)))))))
+
+
+
+
 ;; using the blocks3 idea from Steffan Westcott, translated into transducers
-;; FASTEST pretty good for transducers
+;; pretty good for transducers
+;; about 90 us
 (defn xsudoku-valid? [board]
   (let [transpose (fn [rows] (apply sequence (map list) rows))
         xfail1 (comp (map sort) (remove #(= % [1 2 3 4 5 6 7 8 9])) (take 1))
@@ -72,23 +115,46 @@
                     board))))
 
 
+;; about 52 us
+;; faster with the bit-reduction
+(defn xbs? [board]
+  (let [transpose (fn [rows] (apply sequence (map list) rows))
+        xfail1 (comp (map #(reduce bit-flip 1022 %)) (remove zero?) (take 1))
+        nothing? (fn  ([] nil)  ([r] (nil? r))  ([r x] false))]
+    (and (transduce xfail1 nothing? board)
+         (transduce xfail1 nothing? (transpose board))
+         (transduce (comp (partition-all 3)
+                          (mapcat transpose)
+                          cat
+                          (partition-all 9)
+                          xfail1)
+                    nothing?
+                    board))))
 
-;; similar to mine but I'm lazier, I think, and faster.
-;; sudoku-valid?
-(defn g7s-valid?
-       [rows]
-       (let [valid   #(= (sort %) (range 1 10))
-             slice-3 (partial partition 3)
-             cols    (apply map vector rows)
-             |3x3|   (->> rows
-                          (map slice-3)
-                          slice-3
-                          (mapcat #(apply map concat %)))]
-         (every? valid (concat rows cols |3x3|))))
+(def ^:const sud-bits 1022)
+
+(defn xbs3? [board]
+  (let [transpose (fn [rows] (apply sequence (map list) rows))
+        xfail1 (comp (map #(reduce bit-flip sud-bits %)) (remove zero?) (take 1))
+        nothing? (fn  ([] nil)  ([r] (nil? r))  ([r x] false))]
+    (and (transduce xfail1 nothing? board)
+         (transduce xfail1 nothing? (transpose board))
+         (transduce (comp (partition-all 3)
+                          (mapcat transpose)
+                          cat
+                          (partition-all 9)
+                          xfail1)
+                    nothing?
+                    board))))
+
+
+
+
 
 
 ;; hacked version of Steffan Westcott, singled function
 ;; concise and clever but slow
+;; inspired me to do a transducer version that was pretty good
 (defn westcott-valid? [mat]
   (let [transpose (fn [mat] (apply mapv vector mat))
         blocks3 (fn [mat] (->> mat (map #(partition 3 %)) transpose flatten (partition 9)))
@@ -97,7 +163,7 @@
 
 
 
-
+;; slowish
 (defn sudval? [board]
   (and (every? valid-row? board)
        (let [flat (sequence cat board)]
@@ -107,88 +173,6 @@
                    (sequence cat (for [i (range 3)]
                                    (map (fn [abc] (reduce into [] abc))
                                         (partition 3 (take-nth 3 (drop i threes)))))))))))
-
-
-(defn sudval? [board]
-  (and (every? valid-row? board)
-       (let [flat (sequence cat board)]
-         (every? valid-row? (map (fn [i] (take-nth 9 (drop i flat))) (range 9)))
-         (let [threes (partition 3 flat)]
-           (every? valid-row?
-                   (sequence cat (for [i (range 3)]
-                                   (map (fn [abc] (reduce into [] abc))
-                                        (partition 3 (take-nth 3 (drop i threes)))))))))))
-
-
-(defn ssq1 []
-  (let [flat (vec (range 81))
-        threes (partition 3 flat)]
-    (sequence cat (for [i (range 3)]
-                    (map (fn [abc] (reduce into [] abc))
-                         (partition 3 (take-nth 3 (drop i threes))))))))
-
-
-(defn ssq-bit-faster []
-  (let [flat (vec (range 81))
-        threes (partition 3 flat)]
-    (map #(sequence cat %)
-         (mapcat (fn [i]
-                   (partition 3 (take-nth 3 (drop i threes))))
-         (range 3)))))
-
-
-(defn ssq-more-faster []
-  (let [flat (vec (range 81))
-        threes (partition 3 flat)]
-    (into ()  (comp
-               (mapcat (fn [i]
-                         (into () (comp (drop i) (take-nth 3) (partition-all 3)) threes)))
-               (map #(sequence cat %)))
-          (range 3))))
-
-
-
-(defn ssq []
-  (let [flat (vec (range 81))
-        threes (partition 3 flat)]
-    (mapcat (fn [x3]
-              (into ()
-                    (comp
-                     (take-nth 3)
-                     (partition-all 3)
-                     (map (fn [x33] (sequence cat x33))))
-                    x3))
-            (list threes (next threes) (nnext threes)))))
-
-
-
-
-
-(defn ssqx []
-  (let [flat (vec (range 81))
-        xform (comp
-               (partition-all 3)
-               (take-nth 3)
-               (partition-all 3)
-               (map #(apply concat %))
-               #_(map #(sequence cat %))
-               )
-        ]
-    (mapcat #(into () xform %)
-            (take 3 (iterate #(drop 3 %) flat)))))
-
-
-(defn ssqx2 []
-  (let [flat (vec (range 81))
-        xform (comp
-               (partition-all 3)
-               (take-nth 3)
-               (partition-all 3)
-               (map #(reduce into [] %))
-               )
-        ]
-    (mapcat #(into () xform %)
-            (take 3 (iterate #(drop 3 %) flat)))))
 
 
 
@@ -231,6 +215,17 @@
            [ 6 2 5 9 4 8 1 3 7 ]
            [ 8 7 3 5 1 2 9 6 4 ]])
 
+(def dbad [[ 1 2 5 4 8 9 3 7 6 ]
+           [ 7 3 2 9 5 6 8 4 1 ]
+           [ 4 6 8 3 7 1 2 9 5 ]
+           [ 3 8 7 1 2 4 6 5 9 ]
+           [ 5 9 1 7 6 3 4 2 8 ]
+           [ 2 4 6 8 9 5 7 1 3 ]
+           [ 9 1 4 6 3 7 5 8 2 ]
+           [ 6 5 9 2 4 8 1 3 7 ]
+           [ 8 7 3 5 1 2 9 6 4 ]])
+
+
 (def tgood (transpose good))
 
 (def bad  [[ 1 1 2 4 8 9 3 7 6 ]
@@ -252,12 +247,21 @@
   ([sudoku-valid?]
    (assert (sudoku-valid? good))
    (assert (sudoku-valid? tgood))
+   (assert (not (sudoku-valid? dbad)))
    (assert (not (sudoku-valid? bad) ))
    (assert (not (sudoku-valid? tbad) ))
    true))
 
+(require 'criterium.core)
 
-
+(defn ben
+  ([] (ben sudoku-valid?))
+  ([& svfns]
+   (doseq [sv svfns]
+     (println)
+     (println (str sv))
+     (criterium.core/quick-bench (smoke-sud sv)))
+   (println)))
 
 
 
@@ -266,3 +270,262 @@
 (defn valid-row-slower? [v]
   (= (set v) #{1 2 3 4 5 6 7 8 9}))
 
+
+
+
+
+
+;;;;;
+
+
+(def init-tracker  (into [] (repeat 9 (vector-of :long 0 0 0))))
+
+(defn g7s-fastest?
+  [rows]
+  (let [tracker
+        (persistent!
+         (reduce
+          (fn [tracker [^long row-idx ^long col-idx]]
+            (let [tr-idx        (dec ^long (nth (nth rows row-idx) col-idx))
+                  box-idx       (+ (quot col-idx 3)
+                                   (* (quot row-idx 3) 3))
+                  [row col box] (get tracker tr-idx)]
+              (if (or (bit-test row row-idx)
+                      (bit-test col col-idx)
+                      (bit-test box box-idx))
+                (reduced (conj! tracker false))
+                (assoc! tracker
+                        tr-idx
+                        (vector-of :long
+                                   (bit-set row row-idx)
+                                   (bit-set col col-idx)
+                                   (bit-set box box-idx))))))
+          (transient init-tracker)
+          (for [row-idx (range 0 9) col-idx (range 0 9)]
+            [row-idx col-idx])))]
+    (boolean (peek tracker))))
+
+
+
+;; doesn't matter much but type hints help a little
+(set! *unchecked-math* :warn-on-boxed)
+
+
+;; SEM idea condense 3 elements into one long
+(def init-long-bits (into [] (repeat 9 0)))
+
+(defn sem-fastest1?
+  [rows]
+  (let [tracker
+        (persistent!
+         (reduce
+          (fn [tracker [^long row-idx ^long col-idx]]
+            (let [tr-idx        (dec ^long (nth (nth rows row-idx) col-idx))
+                  box-bit       (+ 18 (quot col-idx 3)
+                                   (* (quot row-idx 3) 3))
+                  col-bit (+ 9 col-idx)
+                  tbits (nth tracker tr-idx)]
+              (if (or (bit-test tbits row-idx)
+                      (bit-test tbits col-bit)
+                      (bit-test tbits box-bit))
+                (reduced (conj! tracker false))
+                (assoc! tracker
+                        tr-idx
+                        (-> tbits (bit-set row-idx) (bit-set col-bit) (bit-set box-bit))))))
+          (transient init-long-bits)
+          (for [row-idx (range 0 9) col-idx (range 0 9)]
+            [row-idx col-idx])))]
+    (boolean (peek tracker))))
+
+(defn sem-fastest?
+  [rows]
+  (let [tracker
+        (persistent!
+         (reduce
+          (fn [tracker [^long row-idx ^long col-idx]]
+            (let [tr-idx        (dec ^long (nth (nth rows row-idx) col-idx))
+                  box-bit       (+ 18 (quot col-idx 3)
+                                   (* (quot row-idx 3) 3))
+                  col-bit (+ 9 col-idx)
+                  tbits (nth tracker tr-idx)]
+              (if (or (bit-test tbits row-idx)
+                      (bit-test tbits col-bit)
+                      (bit-test tbits box-bit))
+                (reduced (conj! tracker false))
+                (assoc! tracker
+                        tr-idx
+                        (-> tbits (bit-set row-idx) (bit-set col-bit) (bit-set box-bit))))))
+          (transient init-long-bits)
+          (for [row-idx (range 0 9) col-idx (range 0 9)]
+            [row-idx col-idx])))]
+    (boolean (peek tracker))))
+
+
+
+
+
+
+(defn sem-svb1 [board]
+  ;; check rows fast
+  (reduce-kv (fn [res ^long irow row]
+               (if-not (zero? ^long (reduce bit-flip 1022 row))
+                 (reduced false)
+                 (reduce-kv (fn [r ^long icol x]
+                              (let [ibox (+ 10 (quot icol 3) (* (quot irow 3) 3))
+                                    bits (nth r x)]
+                                (if (or (bit-test bits icol)
+                                        (bit-test bits ibox))
+                                  (reduced (reduced false))
+                                  (assoc r x (-> bits (bit-set icol) (bit-set ibox))))))
+                            res
+                            row)))
+             (vec (repeat 10 0))
+             board))
+
+(defn sem-svb11 [board]
+  ;; check rows fast
+  (reduce-kv (fn [res ^long irow row]
+               (if-not (zero? ^long (reduce bit-flip 1022 row))
+                 (reduced false)
+                 (reduce-kv (fn [r ^long icol x]
+                              (let [ibox (+ 10 (quot icol 3) (* (quot irow 3) 3))
+                                    bits (nth r x)]
+                                (if (or (bit-test bits icol)
+                                        (bit-test bits ibox))
+                                  (reduced (reduced false))
+                                  (assoc! r x (-> bits (bit-set icol) (bit-set ibox))))))
+                            res
+                            row)))
+             (transient (vec (repeat 10 0)))
+             board))
+
+
+(defn sem-svb2 [board]
+  ;; check rows fast
+  (reduce-kv (fn [res ^long irow row]
+               (reduce-kv (fn [r ^long icol x]
+                            (let [ibox (+ 10 (quot icol 3) (* (quot irow 3) 3))
+                                  col-bit (+ 20 icol)
+                                  bits (nth r x)]
+                                (if (or (bit-test bits irow)
+                                        (bit-test bits col-bit)
+                                        (bit-test bits ibox))
+                                  (reduced (reduced false))
+                                  (assoc r x (-> bits
+                                                 (bit-set irow)
+                                                 (bit-set col-bit)
+                                                 (bit-set ibox))))))
+                            res
+                            row))
+             (vec (repeat 10 0))
+             board))
+
+
+(defn sem-svb? [board]
+  (boolean (reduce-kv (fn [res ^long irow row]
+               (reduce-kv (fn [r ^long icol x]
+                            (let [ibox (+ 10 (quot icol 3) (* (quot irow 3) 3))
+                                  col-bit (+ 20 icol)
+                                  bits (nth r x)]
+                                (if (or (bit-test bits irow)
+                                        (bit-test bits col-bit)
+                                        (bit-test bits ibox))
+                                  (reduced (reduced false))
+                                  (assoc! r x (-> bits
+                                                 (bit-set irow)
+                                                 (bit-set col-bit)
+                                                 (bit-set ibox))))))
+                            res
+                            row))
+             (transient (vec (repeat 10 0)))
+             board)))
+
+
+
+(defn sem-svb4? [board]
+  (boolean (reduce-kv
+            (fn [res ^long irow row]
+              (reduce-kv (fn [res ^long col x]
+                           (let [ibox (+ 16 (quot col 3) (* (quot irow 3) 3))
+                                 icol (+ 32 col)
+                                 bits (nth res x)]
+                             (if (or (bit-test bits irow)
+                                     (bit-test bits icol)
+                                     (bit-test bits ibox))
+                               (reduced (reduced false))
+                               (assoc! res x (-> bits
+                                                 (bit-set irow)
+                                                 (bit-set icol)
+                                                 (bit-set ibox))))))
+                         res
+                         row))
+            (transient (vec (repeat 10 0)))
+            board)))
+
+
+(defn sem-svb5? [board]
+  (boolean (reduce-kv
+            (fn [res ^long irow row]
+              (reduce-kv (fn [res ^long col x]
+                           (let [ibox (+ 16 (quot col 3) (* (quot irow 3) 3))
+                                 icol (+ 32 col)
+                                 bits (nth res x)]
+                             (if (or #_ (not (<= 1 x 9))
+                                     (bit-test bits irow)
+                                     (bit-test bits icol)
+                                     (bit-test bits ibox))
+                               (reduced (reduced false))
+                               (assoc! res x (-> bits
+                                                 (bit-set irow)
+                                                 (bit-set icol)
+                                                 (bit-set ibox))))))
+                         res
+                         row))
+            (transient (vec (repeat 10 0)))
+            board)))
+
+
+
+
+
+;; bit-or with 1 shifts is faster than bit-sets
+
+;; faster with bit twiddling.  Assumes on 1-9 values on a 9x9 board
+(defn sudoku-valid-bits? [board]
+  (boolean (reduce-kv
+            (fn [res ^long r row]
+              (reduce-kv (fn [res ^long c x]
+                           (let [mask ^long (bit-or (bit-shift-left 1 (+ 16 r))
+                                                    (bit-shift-left 1 (+ 32 c))
+                                                    (bit-shift-left 1 (+ (quot c 3)
+                                                                         (* (quot r 3) 3))))
+                                 bits ^long (nth res x)]
+                             (if (zero? (bit-and mask bits))
+                               (assoc! res x (bit-or mask bits))
+                               (reduced (reduced false)))))
+                         res
+                         row))
+            (transient (vec (repeat 10 0)))
+            board)))
+
+
+
+
+(set! *unchecked-math* false)
+
+(defn svbits? [board]
+  (boolean (reduce-kv
+            (fn [res ^long r row]
+              (reduce-kv (fn [res ^long c x]
+                           (let [mask ^long (bit-or (bit-shift-left 1 (+ 16 r))
+                                                    (bit-shift-left 1 (+ 32 c))
+                                                    (bit-shift-left 1 (+ (quot c 3)
+                                                                         (* (quot r 3) 3))))
+                                 bits ^long (nth res x)]
+                             (if (zero? (bit-and mask bits))
+                               (assoc! res x (bit-or mask bits))
+                               (reduced (reduced false)))))
+                         res
+                         row))
+            (transient (vec (repeat 10 0)))
+            board)))
