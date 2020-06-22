289750f385b2d47e823b15c1be1cbf9a17f47022
Steve Miner <steveminer@gmail.com>
2020-06-22 16:37:40 -0400

    clean up uniqs

--- src/miner/euniq.clj
+++ src/miner/euniq.clj
@@ -1,12 +1,16 @@
 (ns miner.euniq)
 
+;; https://gist.github.com/ericnormand/235696a7f7cca803f772a2d1bd1b90c3
+;; Note that the uniques are supposed to be in the order of the original coll.  That forces
+;; a bit of extra work.
+
 ;; best
 (defn uniques [coll]
   (let [freqs (frequencies coll)]
     (sequence (filter (fn [x] (= (get freqs x) 1))) coll)))
 
 
-;; fastest
+;; not much faster
 ;; incorporate frequencies but skip persistent! to save time.
 (defn uni4 [coll]
   (let [freqs (reduce (fn [counts x]
@@ -26,6 +30,7 @@
   (let [freqs (frequencies coll)]
     (filter (fn [x] (= (get freqs x) 1)) coll)))
 
+;; fastest, not lazy at all
 (defn uniques1 [coll]
   (let [freqs (frequencies coll)]
     (into [] (filter (fn [x] (= (get freqs x) 1))) coll)))
@@ -46,4 +51,6 @@
    (assert (= (uniques [1 2 3 4 5 6 1 2 3 5 6]) '(4)))
    (assert (= (uniques [:a :b :c :c]) '(:a :b)))
    (assert (= (uniques [1 2 3 1 2 3]) ()))
+   ;; espensive test but shows that original ordering matters
+   (assert (= (uniques (cons 19 (range 20))) (range 19)))
    true))
