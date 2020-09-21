06b461639d27b77020670dfc239e5ef1128828c6
Steve Miner <steveminer@gmail.com>
2020-09-21 15:21:44 -0400

    better seasons

--- src/miner/eseason.clj
+++ src/miner/eseason.clj
@@ -13,31 +13,27 @@
 
 
 
-;; SEM:  note `day` is not needed, according to that table
-
-(defn northern [month day]
-  (case month
-    (:march :april :may) :spring
-    (:june :july :august) :summer
-    (:september :october :november) :autumn
-    (:december :january :february) :winter))
-
-(defn southern [month day]
-  (case month
-    (:march :april :may) :autumn
-    (:june :july :august) :winter
-    (:september :october :november) :spring
-    (:december :january :february) :summer))
+;; SEM:  note `day` is not needed, according to that table.
+;; I did another version lower down that uses more precise dates.
 
 
-(defn which-season1 [hemi month day]
-  (let [fhemi (if (= hemi :south) southern northern)]
-    (fhemi month day)))
+;; As we don't expect to change months or seasons, we can use a literal translation.
+(defn which-season [hemi month day]
+  (if (= hemi :south)
+    (case month
+      (:march :april :may) :autumn
+      (:june :july :august) :winter
+      (:september :october :november) :spring
+      (:december :january :february) :summer)
+    (case month
+      (:march :april :may) :spring
+      (:june :july :august) :summer
+      (:september :october :november) :autumn
+      (:december :january :february) :winter)))
 
 
-;; slightly faster, pretty literal
 ;; SEM added arities for convenience, not part of problem description
-(defn which-season
+(defn which-season2
   ([month] (which-season :north month 1))
   ([month day] (which-season :north month day))
   ([hemi month day]
@@ -57,10 +53,6 @@
 
 
 
-
-
-
-
 (defn smoke-season [which-season]
   (assert (= (which-season :north :march 5) :spring))
   (assert (= (which-season :south :december 25) :summer))
@@ -71,10 +63,8 @@
 
 
 
-;;; SEM: But Eric does not have the correct dates for the seasons.
-
-;;; The 21st of the month is a better average day for the soltices.  These are better dates,
-;;; and make a more interesting problem.
+;;; SEM: It's a better problem if the days matter.  The 21st of the month is a better
+;;; average day for the soltices.  These are better dates:
 
 ;; Start        End          North  South
 ;; March 20     June 20      Spring Autumn
@@ -83,40 +73,22 @@
 ;; December 21  March 19     Winter Summer
 
 
-
-(def months [:january :february :march :april :may :june :july :august :september :october
-             :november :december])
-
-(def month-indices (into {} (map-indexed #(vector %2 (* 100 (inc %1))) months)))
-
+;;; MMDD for convenient literal longs
 (defn date-index [month day]
-  (+ (get month-indices month) day))
-
-
-(def raw-season-data [[:march 20 :june 20 :spring :autumn]
-                      [:june 21 :september 22 :summer :winter]
-                      [:september 23 :december 20 :autumn :spring]
-                      [:december 21 :march 19 :winter :summer]])
-
-(defn between? [a b x]
-  (<= a x b))
-
-
-(defn season [hemi month day]
-  (let [ind (date-index month day)]
-    (if (= hemi :south)
-      (cond (between? 101 319 ind) :summer
-            (between? 320 620 ind) :autumn
-            (between? 621 922 ind) :winter
-            (between? 923 1220 ind) :spring
-            (between? 1221 1231 ind) :summer)
-      (cond (between? 101 319 ind) :winter
-            (between? 320 620 ind) :spring
-            (between? 621 922 ind) :summer
-            (between? 923 1220 ind) :autumn
-            (between? 1221 1231 ind) :winter))))
-
-
+  (+ day
+     (case month
+       :january 100
+       :february 200
+       :march 300
+       :april 400
+       :may 500
+       :june 600
+       :july 700
+       :august 800
+       :september 900
+       :october 1000
+       :november 1100
+       :december 1200)))
 
 
 (defn season2 [hemi month day]
@@ -151,4 +123,44 @@
         621 :summer
         320 :spring
         :winter))))
+
+
+
+
+
+
+
+;; Older junk
+
+(def months [:january :february :march :april :may :june :july :august :september :october
+             :november :december])
+
+(def month-indices (into {} (map-indexed #(vector %2 (* 100 (inc %1))) months)))
+
+(defn date-index1 [month day]
+  (+ (get month-indices month) day))
+
+
+(def raw-season-data [[:march 20 :june 20 :spring :autumn]
+                      [:june 21 :september 22 :summer :winter]
+                      [:september 23 :december 20 :autumn :spring]
+                      [:december 21 :march 19 :winter :summer]])
+
+(defn between? [a b x]
+  (<= a x b))
+
+
+(defn season [hemi month day]
+  (let [ind (date-index month day)]
+    (if (= hemi :south)
+      (cond (between? 101 319 ind) :summer
+            (between? 320 620 ind) :autumn
+            (between? 621 922 ind) :winter
+            (between? 923 1220 ind) :spring
+            (between? 1221 1231 ind) :summer)
+      (cond (between? 101 319 ind) :winter
+            (between? 320 620 ind) :spring
+            (between? 621 922 ind) :summer
+            (between? 923 1220 ind) :autumn
+            (between? 1221 1231 ind) :winter))))
 
