bb3bc5152b4f37599e1065f53431becc123213d6
Steve Miner <steveminer@gmail.com>
2020-09-28 16:02:56 -0400

    wip wolf

---
+++ src/miner/ewolf.clj
@@ -0,0 +1,121 @@
+(ns miner.ewolf)
+
+;; https://gist.github.com/ericnormand/3bcffa74da2cad97fcbfb908e347bcde
+
+;; Eric's Challenge
+;; Wolf-Sheep-Cabbage puzzle
+;;
+
+;; You own a wolf, a sheep, and a cabbage. You need to cross a river and you only have a
+;; boat big enough for you plus one other passenger (your wolf, sheep, or cabbage). The
+;; trouble is, the animals are hungry. If you leave the wolf with the sheep, the wolf will
+;; eat it. If you leave the sheep with the cabbage, it will eat it. The wolf, being a
+;; carnivore, will not eat the cabbage. How do you move them safely to the other side with
+;; nothing being eaten?
+;;
+;; Your task is to write a function that generates solutions to this puzzle, in the form of
+;; boat crossings. Each crossing should state: the direction you are going, and the
+;; passenger in your boat.  Bonus: Write a function that validates a solution.
+
+;; (defn wolf-sheep-cabbage []) => ([...
+;;                                  {:direction :across
+;;                                   :passenger :cabbage}
+;;                                  {:direction :return
+;;                                   :passenger :sheep}]
+;;                                 ...)
+
+;; inital state:  W S C b ---  [empty]
+;; :across S,   W C --- b S
+;; :return nil, W C b --- S
+;;:across C,  W --- b C S
+;; :return S,  W S b --- C
+;; :across W,  S -- b W C
+;; :return nil, S b -- W C
+;; :across S, [empty] -- b S W C
+
+
+;; :across W,  S --- b W C
+;; :return nil, S b -- W C
+;; :across S, [empty] -- b S W C
+
+;; Assume You and Boat are always together.  The animals eat something only if you leave
+;; them alone together.  If you're there, everyone is safe.
+
+;; no passenger is that same as :passager :boat, and that seems the more convenient notation
+
+;; Four characters:
+(def characters [:boat :sheep :wolf :cabbage])
+
+(def all (set characters))
+
+(def eaters [#{:wolf :sheep} #{:sheep :cabbage}])
+
+(def opposite-dir {:across :return :return :across})
+
+(defn subset? [sub super]
+  (every? super sub))
+
+;; state {:return #{characters} :across #{characters}}
+
+(defn illegal-state? [state]
+  (let [start (:return state)
+        destination (:across state)
+        non-boat (if (:boat start) destination start)]
+    (not-any? #(subset? % non-boat) eaters)))
+
+
+(defn illegal-side? [side]
+  (and (:sheep side)
+       (or (:wolf side)
+           (:cabbage side))))
+
+(defn legal-moves [dir boatside]
+  (vec (for [passenger boatside :when (not (illegal-side? (disj boatside passenger)))]
+         {:direction dir
+          :passenger passenger})))
+
+(defn state-legal-moves [state]
+  (let [start (:return state)
+        destination (:across state)
+        dir (if (:boat start) :across :return)
+        boatside (if (:boat start) start destination)]
+    (legal-moves dir boatside)))
+
+(defn solution? [state]
+  (= all (:across state)))
+
+(defn execute-first-move [state stack]
+  (if (> (count stack) 200)
+    (throw (ex-info "Stack overflow" {:stack stack}))
+    (let [possibles (:possible-moves state)
+          move (peek possibles)]
+      (if-not move
+        stack
+        (let [passenger (:passenger move)
+              dir (:direction move)]
+          (conj stack
+                (update state :possible-moves pop)
+                (let [boat-side (conj (dir state) :boat passenger)]
+                  (-> state
+                      (assoc :after move)
+                      (update (opposite-dir dir) disj :boat passenger)
+                      (assoc dir boat-side)
+                      (assoc :possible-moves (legal-moves (opposite-dir dir) boat-side))))))))))
+
+
+
+;; Finds one solution and quits
+(defn wsc []
+  (loop [stack [{:return all :across #{} :after nil :possible-moves (legal-moves :across all)}]]
+    (let [state (peek stack)]
+      (println state)
+      (cond (nil? state) nil
+            (some #(= (:return state) %) (map :return (pop stack)))
+                (do (println "REPEAT") (recur (pop stack)))
+            (solution? state)  (into () (map :after) stack)
+            ;; SEM FIXME don't need to check this
+            (empty? (:possible-moves state))
+                (do (println "EMPTY MOVES") (recur (pop stack)))
+            :else (recur (execute-first-move state (pop stack)))))))
+
+
