0eefa8ce511b6b809ba5c19e5b0af3da4a12e55c
Steve Miner <steveminer@gmail.com>
2020-10-26 13:56:03 -0400

    sums of pairs

--- src/miner/esumpair.clj
+++ src/miner/esumpair.clj
@@ -10,17 +10,71 @@
 ;; Each pair should be sorted.
 
 
+;; Note: mc/combinations doesn't reuse duplicate items so you need to generate the indices
+;; to be unique.
+
+;; we only need index pairs so we can simplify and use a transducer chain
+
+;; fastest
+(defn sums-of-pairs [coll sum]
+  (let [vvv (vec (sort coll))
+        cnt (count vvv)]
+    (into [] (comp (map vector)
+                   (mapcat (fn [prev] (map #(conj prev %) (range (inc (peek prev)) cnt))))
+                   (keep (fn [[i j]] (when (= sum (+ (vvv i) (vvv j)))
+                                       (vector (vvv i) (vvv j))))))
+          (range cnt))))
 
 
 
 
+
+
+
+;; natural way
+(defn sums-of-pairs1 [coll sum]
+  (let [vvv (vec (sort coll))]
+    (keep (fn [[i j]] (when (= sum (+ (vvv i) (vvv j)))
+                        (vector (vvv i) (vvv j))))
+            (mc/combinations (range (count vvv)) 2))))
+
+
+;; eager, not lazy, faster than using clojure.math.combinatorics/combinations
+(defn combo-indices [cnt choose]
+  ;; {:pre [(pos-int? choose) (<= choose cnt)]}
+  (loop [i (unchecked-dec choose) res (map vector (range cnt))]
+    (if (zero? i)
+      res
+      (recur (unchecked-dec i)
+             (mapcat (fn [prev] (map #(conj prev %) (range (inc (peek prev)) cnt))) res)))))
+
+
+(defn sums-of-pairs2 [coll sum]
+  (let [vvv (vec (sort coll))]
+    (keep (fn [[i j]] (when (= sum (+ (vvv i) (vvv j)))
+                        (vector (vvv i) (vvv j))))
+          (combo-indices (count vvv) 2))))
+
+
+;; specialized version of combo-indices for exactly 2
+(defn indices2 [cnt]
+  ;; {:pre [(>= cnt 2)]}
+  (mapcat (fn [prev] (map #(conj prev %) (range (inc (peek prev)) cnt)))
+          (map vector (range cnt))))
+
+
+
+;; errors in examples
+;; should sort results so [0 5] comes before [2 3]
+;; third example should return [2 5] as well
 
 (defn smoke-sum
-  ([sum-of-pairs]
-   (assert
-(sums-of-pairs [2 4 5 6] 8) ;=> [[2 6]]
-(sums-of-pairs [3 2 0 1 1 5] 5) ;=> [[2 3] [0 5]]
-(sums-of-pairs [1 3 2 3 4 5] 7) ;=> [[3 4] [3 4]]
-Notes
+  ([] (smoke-sum sums-of-pairs))
+  ([sums-of-pairs]
+   (assert (= (sums-of-pairs [2 4 5 6] 8) [[2 6]]))
+   (assert (= (sums-of-pairs [3 2 0 1 1 5] 5) [[0 5] [2 3]]))
+   (assert (= (sums-of-pairs [1 3 2 3 4 5] 7) [[2 5] [3 4] [3 4]]))
+   true))
+
 
 
