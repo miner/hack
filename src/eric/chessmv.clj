eb0a4b8a7d3ff6e4b7a1bdbaf11e2480c42df7e6
Steve Miner <steveminer@gmail.com>
2022-01-06 10:35:21 -0500

    chess cleanup

--- src/eric/chessmv.clj
+++ src/eric/chessmv.clj
@@ -13,13 +13,66 @@
 
 
 ;; ----------------------------------------------------------------------
-;; Board positions FR, file A-H, rank 1-8
+;; Board positions "FR", file A-H, rank 1-8
 
 ;; Algebraic Notation for chess
 ;; https://www.thechessdrum.net/chessacademy/thechessboard.html
 
+;; Board origination: "white on right, queen on her color".  Lower right corner is white.
+;; White pieces start at bottom, moving up.  Columns "file" A-H, left to right.  Rows
+;; "rank" (1-8), bottom up.  A1 is lower left, H8 is upper right.
 
+;; Piece names are keywords, like :king.
+;; Positions are two-char strings, like "C5".
 ;; nth is faster for char access than first/second
+
+(defn can-move? [piece a b]
+  (let [file (fn [cc] (case (nth cc 0)
+                        \A 0  \B 1  \C 2  \D 3  \E 4
+                        \F 5  \G 6  \H 7  nil))
+        rank (fn [cc] (case (nth cc 1)
+                        \1 0  \2 1  \3 2  \4 3  \5 4
+                        \6 5  \7 6  \8 7  nil))
+        abs (fn [n] (if (neg? n) (- n) n))]   
+    (when-not (= a b)
+      (when-let [ra (rank a)]
+        (when-let [rb (rank b)]
+          (when-let [fa (file a)]
+            (when-let [fb (file b)]
+              (let [df (- fb fa)
+                    dr (- rb ra)]
+                (case piece
+                  :pawn (and (= dr 1) (zero? df))
+                  :king (and (<= (abs dr) 1) (<= (abs df) 1))
+                  :queen (or (zero? df)
+                             (zero? dr)
+                             (= (abs df) (abs dr)))
+                  :rook (or (zero? df) (zero? dr))
+                  :bishop  (= (abs df) (abs dr))
+                  :knight  (let [ar (abs dr)
+                                 af (abs df)]
+                             (and (= (min ar af) 1)
+                                  (= (max ar af) 2))))))))))))
+
+
+
+
+(defn smoke-cm [can-move?]
+  (assert (can-move? :pawn "A2" "A3"))
+  (assert (not (can-move? :pawn "A2" "B2")))
+  (assert (can-move? :queen "H1" "A8"))
+  (assert (not (can-move? :knight "A4" "A5")))
+  (assert (can-move? :knight "A4" "C5"))
+  (assert (not (can-move? :king "A8" "A9"))) ; (that's off the board)
+  (assert (not (can-move? :rook "A4" "A4")))
+  (assert (can-move? :rook "A4" "A8"))
+  (assert (can-move? :rook "B5" "F5"))
+  (assert (can-move? :bishop "H1" "A8"))
+  (assert (not (can-move? :bishop "H2" "A8")))
+  true)
+
+
+
 
 (defn file [cc]
   (case (nth cc 0)
@@ -54,7 +107,7 @@
 (defmulti dmove? (fn [piece df dr] piece))
 
 ;; most of the logic is common, just specialized on the deltas
-(defn can-move? [piece a b]
+(defn can-move-GOOD? [piece a b]
   (when-not (= a b)
     (let [ra (rank a)
           rb (rank b)
@@ -96,47 +149,3 @@
          (= (max ar af) 2))))
 
         
-
-
-
-(defn smoke-cm [can-move?]
-  (assert (can-move? :pawn "A2" "A3"))
-  (assert (not (can-move? :pawn "A2" "B2")))
-  (assert (can-move? :queen "H1" "A8"))
-  (assert (not (can-move? :knight "A4" "A5")))
-  (assert (can-move? :knight "A4" "C5"))
-  (assert (not (can-move? :king "A8" "A9"))) ; (that's off the board)
-  (assert (not (can-move? :rook "A4" "A4")))
-  (assert (can-move? :rook "A4" "A8"))
-  (assert (can-move? :rook "B5" "F5"))
-  (assert (can-move? :bishop "H1" "A8"))
-  (assert (not (can-move? :bishop "H2" "A8")))
-  true)
-
- 
-
-  
-
-
-;; slightly changed into one fn
-(defn sw-can-move? [piece orig dest]
-  (let [sq-coords (fn [sq]
-                    (let [sq' (mapv #(- (int %1) %2) sq [65 49])]
-                      (when (every? #(<= 0 % 7) sq')
-                        sq')))
-        orig-sq (sq-coords orig)
-        dest-sq (sq-coords dest)]
-    (if (and orig-sq dest-sq (not= orig dest))
-      (let [[dx dy] (map - dest-sq orig-sq)
-            same-rank? (zero? dy)
-            same-file? (zero? dx)
-            same-diagonal? (= (* dx dx) (* dy dy))]
-        (case piece
-          :pawn (and same-file? (= 1 dy) (pos? (get orig-sq 1)))
-          :rook (or same-file? same-rank?)
-          :bishop same-diagonal?
-          :queen (or same-file? same-rank? same-diagonal?)
-          :king (every? #(<= -1 % 1) [dx dy])
-          :knight (= 5 (+ (* dx dx) (* dy dy)))
-          false))
-      false)))
