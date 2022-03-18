7d577b22694fad5fee74c6df7c2075c4b2557c52
Steve Miner <steveminer@gmail.com>
2022-03-18 13:04:55 -0400

    test info

---
+++ test/miner/test_hack.clj
@@ -0,0 +1,19 @@
+(ns miner.test-hack
+  (:require [clojure.test :refer [deftest is testing]]
+            [clojure.test.check.generators :as gen]
+            [clojure.spec.alpha :as s]
+            [clojure.string :as str]
+            [miner.strgen :as sg]))
+
+(deftest show-info
+  (testing "Show test info"
+    (println)
+    (println "  ** Test Info **")
+    (let [info (clojure.edn/read-string (slurp  "project.clj"))]
+      (println " " (second info) (nth info 2)))
+    (println "  Clojure" (clojure-version))
+    (println "  Java" (System/getProperty "java.version"))
+    (println "  JDK vendor:" (System/getProperty "jdk.vendor.version"))
+    (println)
+    true))
+
