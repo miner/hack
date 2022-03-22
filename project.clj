03edba337a8ec066d26d3a2d3c99d0d251b8584d
Steve Miner <steveminer@gmail.com>
2022-03-22 13:26:38 -0400

    clj 1.11

--- project.clj
+++ project.clj
@@ -36,7 +36,7 @@
                  [me.raynes/fs "1.4.6"]]
   :global-vars {*warn-on-reflection* true
                 *unchecked-math* :warn-on-boxed}
-  :profiles {:dev  {:dependencies [[org.clojure/clojure "1.11.0-rc1"]
+  :profiles {:dev  {:dependencies [[org.clojure/clojure "1.11.0"]
                                    ;; [org.clojure/core.async "1.5.640"]
                                    ;;[clj-ns-browser "1.3.0" ]
                                    [criterium "0.4.6"]]}
