d1974712eddb9b33eeabffdedef56dde6095c2df
Steve Miner <steveminer@gmail.com>
2022-12-07 08:29:26 -0500

    added dom-top and updated libs

--- project.clj
+++ project.clj
@@ -1,20 +1,21 @@
 (defproject hack "0.3.0-SNAPSHOT" 
   :min-lein-version "2.0.0"
-  :dependencies [[djblue/portal "0.19.2"]
+  :dependencies [[djblue/portal "0.35.0"]
+                 [dom-top "1.0.8"]
                  [racehub/om-bootstrap "0.6.1" :exclusions [org.clojure/clojure]]
                  [com.velisco/wilkins "0.2.1"]
-                 [com.velisco/strgen "0.2.1"]
+                 [com.velisco/strgen "0.2.4"]
                  [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/data.int-map "1.0.0"]
                  [org.clojure/data.avl "0.1.0"]
                  [org.clojure/core.rrb-vector "0.1.2"]
-                 [org.clojure/core.async "1.5.648"]
+                 [org.clojure/core.async "1.6.673"]
                  [org.clojure/tools.macro "0.1.5"]
                  [org.clojure/data.priority-map "1.1.0"]
                  [com.datomic/datomic-free "0.9.5697"  :exclusions [joda-time]]
-                 [instaparse "1.4.10"]
+                 [instaparse "1.4.12"]
                  ;;[clj-webdriver "0.5.1"]
-                 [org.clojure/core.logic "1.0.0"]
+                 [org.clojure/core.logic "1.0.1"]
                  [quil "3.1.0"]
                  [primitive-math "0.1.6"]
                  [com.velisco/herbert "0.7.0"]
@@ -25,15 +26,15 @@
                  [org.clojure/tools.macro "0.1.5"]
                  [backtick "0.3.4"]
                  [com.velisco/halfbaked "0.2.3"]
-                 [com.velisco/clj-ftp "0.3.15"]
-                 [com.clojure-goes-fast/clj-memory-meter "0.1.3"]
-                 [com.clojure-goes-fast/clj-java-decompiler "0.3.1"]
+                 [com.velisco/clj-ftp "0.3.17"]
+                 [com.clojure-goes-fast/clj-memory-meter "0.2.1"]
+                 [com.clojure-goes-fast/clj-java-decompiler "0.3.2"]
                  [metrics-clojure "2.10.0"]
-                 [net.cgrand/xforms "0.19.2"]
+                 [net.cgrand/xforms "0.19.4"]
                  [digest "1.4.10"]
                  ;; [org.clojure/spec.alpha "0.3.214"]
                  ;; [it.unimi.dsi/sux4j "3.0.10"] ;perfect-hashing
-                 [clj-commons/fs "1.6.307"]]
+                 [clj-commons/fs "1.6.310"]]
   :global-vars {*warn-on-reflection* true
                 *unchecked-math* :warn-on-boxed}
   :profiles {:dev  {:dependencies [[org.clojure/clojure "1.12.0-alpha1"]
@@ -48,7 +49,7 @@
                                    [org.clojure/clojure "1.7.0"]
                                    ]}
              :rebl {:dependencies [
-                                   [org.clojure/core.async "1.5.648"]
+                                   [org.clojure/core.async "1.6.673"]
                                    [REBL "0.9.220"]
                                    ]
                     :main cognitect.rebl}
