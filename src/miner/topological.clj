91e20d943571b4e1dc4395a7cd08c2170b701597
Steve Miner <steveminer@gmail.com>
2023-05-06 22:23:36 -0400

    more topo

--- src/miner/topological.clj
+++ src/miner/topological.clj
@@ -8,6 +8,10 @@
 ;;; its vertices such that for every directed edge uv from vertex u to vertex v, u comes
 ;;; before v in the ordering
 
+;;; Most of these solutions are based on Kahn's algorithm.  There is another way of doing it
+;;; using depth first search.  (Not implemented here.)  My version is sem-topo which is
+;;; significantly faster due to slightly rearranged code.
+
 
 ;;; from Rosetta Code
 (defn topological-sort [graph]
@@ -22,15 +26,23 @@
                             [k (apply disj v depless)]))
                      (apply dissoc graph depless)))))))
 
+;;; The examples are dependecy graphs.  For example, if A depends on B, C and D, it should
+;;; not come before any of them in the result of the topological sort.  There may be more
+;;; than one acceptable answer.
+
 (def ggg {:a #{:b :c :d}
-              :b #{:c :d}
-              :c #{:d :e}
-              :d #{}
+          :b #{:c :d}
+          :c #{:d :e}
+          :d #{}
           :e #{:d}})
+
+;;; => [:d :e :c :b :a]
 
 (def circ {:a #{:b}
            :b #{:c}
            :c #{:a}})
+
+;;; => nil (failure)
 
 
 (defn test-topo [topological-sort]
@@ -60,17 +72,8 @@
           (recur (into res ks) m))))))
 
 
-;;; Fastest and my favorite
+;;; almost Fastest and my favorite
 (defn sem-topo [graph]
-  (loop [tks [] g graph]
-    (if (seq g)
-      (when-let [ks (reduce-kv (fn [ks k v] (if (seq v) ks (conj ks k))) nil g)]
-        (recur (into tks ks)
-               (update-vals (reduce dissoc g ks) #(reduce disj % ks))))
-      tks)))
-
-;; similar to fastest
-(defn sem-topo1 [graph]
   (loop [tks [] g graph]
     (if (empty? g)
       tks
@@ -80,22 +83,29 @@
 
 
 
-(defn sem-topo2 [graph]
-  (loop [tks [] g graph]
-    (if (seq g)
-      (when-let [ks (reduce-kv (fn [ks k v] (if (seq v) ks (conj ks k))) nil g)]
-        (recur (into tks ks)
-               (persistent!
-                (reduce-kv (fn [m k v] (assoc! m k (reduce disj v ks)))
-                           (reduce dissoc! (transient g) ks)))))
-      tks)))
+;;; Based on my literal translation of Kahn's algo.
+;;; Much faster to use vector S instead of set.  Fastest version now.
+(defn kahn-topo [graph]
+  (let [conj-k-when-empty (fn [ks k v] (if (seq v) ks (conj ks k)))]
+    (loop [s (reduce-kv conj-k-when-empty [] graph)
+           tks []
+           g graph]
+      (if-let [n (peek s)]
+        (let [g (update-vals (dissoc g n) #(disj % n))]
+          (recur (reduce-kv conj-k-when-empty (pop s) g)
+                 (conj tks n)
+                 g))
+        (when (empty? g)
+          tks)))))
+
+
 
 
 
 
 
 ;;; Slower to build up intermediate ks as a set
-(defn sem-topo2 [graph]
+(defn sem-topo3 [graph]
   (loop [tks [] g graph]
     (if (empty? g)
       tks
@@ -104,3 +114,32 @@
           (recur (into tks ks)
                  (update-vals (reduce dissoc g ks) #(clojure.set/difference % ks))))))))
 
+
+;; m is "marked" map, perm true, temp false, unmarked if not present
+;; special key ::tks has the vector of nodes in topo order
+
+(defn df-visit1 [g m n]
+  (cond (nil? m) nil
+        (true? (get m n)) m
+        (false? (get m n)) nil
+        :else (let [m2 (assoc m n false)
+                    m3 (reduce (fn [m n] (df-visit1 g m n)) m2 (get g n))]
+                (when m3
+                  (-> m3
+                      (assoc n true)
+                      (update ::tks conj n))))))
+
+;; m is "marked" map, perm true, temp false, unmarked if not present/nil.
+;; special key ::tks has the vector of nodes in topo order.
+;;; refactored df-visit as closure over graph, still recursive.
+
+(defn df-topo [graph]
+  (let [df-visit (fn df-visit [m n]
+                   (when m
+                     (case (get m n)
+                       true m
+                       false nil
+                       (some-> (reduce df-visit (assoc m n false) (get graph n))
+                               (assoc n true)
+                               (update ::tks conj n)))))]
+    (::tks (reduce df-visit {::tks []} (keys graph)))))
