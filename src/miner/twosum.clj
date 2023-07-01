e5b1287f7adec7835be0ba9f1b506c739744eb8d
Steve Miner <steveminer@gmail.com>
2023-07-01 14:54:55 -0400

    twosum int-set

--- src/miner/twosum.clj
+++ src/miner/twosum.clj
@@ -1,5 +1,6 @@
 (ns miner.twosum
-  (:require [clojure.math.combinatorics :as mc]))
+  (:require [clojure.math.combinatorics :as mc]
+            [clojure.data.int-map :as im]))
 
 ;;; Inspired by
 ;;; https://news.ycombinator.com/item?id=36498477
@@ -23,11 +24,11 @@
 
 
 (defn test-twosum [twosum]
-  (assert (= (sort (twosum [1 5 2 6 7 0 3] 5)) '(0 5)))
+  (assert (= (sort (twosum [1 5 12 6 7 0 3] 5)) '(0 5)))
   (assert (= (sort (twosum [1 5 2 6 7 20 3] 5)) '(2 3)))
   (assert (nil? (twosum [1 5 2 6 7 20 3] 15)))
   (assert (= (sort (twosum (cons 1 (range 0 100 2)) 51)) '(1 50)))
-  (assert (= (sort (twosum (concat (range 0 101 2) '(1 3 5)) 103)) '(3 100)))
+  (assert (= (sort (twosum (concat (range 0 101 2) '(1 3 8)) 103)) '(3 100)))
   true)
 
 
@@ -48,9 +49,69 @@
 ;;; perhaps should consider starting indices around half target.
 
 
-;;; ordered-set.  start at half, check for complement, dec
+;;; sorted-set.  start at half, check for complement, dec
+
+;;; Works but slow!
+(defn twosum2 [nums target]
+  (let [vset (into (sorted-set) nums)
+        half (quot (inc target) 2)]
+    (loop [vs (rsubseq vset <= half)]
+      (when-first [v vs] 
+        (if-let [v2 (vset (- target v))]
+          [v v2]
+          (recur (rest vs)))))))
+
+(defn twosum3 [nums target]
+  (let [vset (into (sorted-set) nums)
+        half (quot (inc target) 2)]
+    (loop [vs (subseq vset <= half)]
+      (when-first [v vs] 
+        (if-let [v2 (vset (- target v))]
+          [v v2]
+          (recur (rest vs)))))))
+
+;; im/range not worth it
+(defn twosum4 [nums target]
+  (let [vset (into (im/dense-int-set) nums)
+        half (quot (inc target) 2)]
+    (loop [vs (im/range vset (first vset) half)]
+      (when-first [v vs] 
+        (if-let [v2 (vset (- target v))]
+          [v v2]
+          (recur (rest vs)))))))
+
+;; BUG: what if target is negative?  can you quit at "half"?
+
+;; fastest
+(defn twosum5 [nums target]
+  (let [vset (into (im/dense-int-set) nums)
+        half (quot (inc target) 2)]
+    (loop [vs vset]
+      (let [v (first vs)]
+        (when (and v (< v half))
+          (if-let [v2 (vset (- target v))]
+            [v v2]
+            (recur (rest vs))))))))
+
+(defn twosum6 [nums target]
+  (let [vset (into (im/dense-int-set) nums)
+        vmin (first vset)
+        vmax (first (rseq vset))
+        half (quot (inc target) 2)]
+    (loop [vs vset]
+      (let [v (first vs)]
+        (when (and v (< v half))
+          (let [v2 (- target v)]
+            (if (vset v2)
+              [v v2]
+              (if (> v2 vmax)
+            (recur (rest vs))))))))
 
 
+
+(defn ss3 [nums target]
+  (count (into (sorted-set) nums))
+  (twosum nums target))
 
 #_
 (require '[clojure.math.combinatorics :as mc])
