920ad26ddefc1d9bd6401799a2e310306d75750d
Steve Miner <steveminer@gmail.com>
2023-08-17 16:02:37 -0400

    updated libs +datomic-local

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject hack "0.3.0-SNAPSHOT" 
   :min-lein-version "2.0.0"
-  :dependencies [[djblue/portal "0.40.0"]
+  :dependencies [[djblue/portal "0.45.1"]
                  [dom-top "1.0.8"]
+                 [com.datomic/local "1.0.267"]
                  [racehub/om-bootstrap "0.6.1" :exclusions [org.clojure/clojure]]
                  [com.velisco/wilkins "0.2.1"]
                  [com.velisco/strgen "0.2.4"]
@@ -9,7 +10,7 @@
                  [org.clojure/data.int-map "1.2.1"]
                  [org.clojure/data.avl "0.1.0"]
                  [org.clojure/core.rrb-vector "0.1.2"]
-                 [org.clojure/core.async "1.6.673"]
+                 [org.clojure/core.async "1.6.681"]
                  [org.clojure/tools.macro "0.1.5"]
                  [org.clojure/data.priority-map "1.1.0"]
                  [com.datomic/datomic-free "0.9.5697"  :exclusions [joda-time]]
@@ -27,10 +28,10 @@
                  [backtick "0.3.4"]
                  [com.velisco/halfbaked "0.2.3"]
                  [com.velisco/clj-ftp "1.1.0"]
-                 [com.clojure-goes-fast/clj-memory-meter "0.2.3"]
+                 [com.clojure-goes-fast/clj-memory-meter "0.3.0"]
                  [com.clojure-goes-fast/clj-java-decompiler "0.3.4"]
                  [metrics-clojure "2.10.0"]
-                 [net.cgrand/xforms "0.19.4"]
+                 [net.cgrand/xforms "0.19.5"]
                  [digest "1.4.10"]
                  ;; [org.clojure/spec.alpha "0.3.214"]
                  ;; [it.unimi.dsi/sux4j "3.0.10"] ;perfect-hashing
@@ -50,7 +51,7 @@
                                    [org.clojure/clojure "1.7.0"]
                                    ]}
              :rebl {:dependencies [
-                                   [org.clojure/core.async "1.6.673"]
+                                   [org.clojure/core.async "1.6.681"]
                                    [REBL "0.9.220"]
                                    ]
                     :main cognitect.rebl}
