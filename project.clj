6c2341aa37914853752feea4e42701ab4d42a9d4
Steve Miner <steveminer@gmail.com>
2023-08-19 14:31:31 -0400

    local is good enough

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[djblue/portal "0.45.1"]
                  [dom-top "1.0.8"]
                  [com.datomic/local "1.0.267"]
-                 [com.datomic/client-api "1.0.67"]
+                 ;;[com.datomic/client-api "1.0.67"]
                  [racehub/om-bootstrap "0.6.1" :exclusions [org.clojure/clojure]]
                  [com.velisco/wilkins "0.2.1"]
                  [com.velisco/strgen "0.2.4"]
