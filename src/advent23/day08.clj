00cbc054e11818137e6b8fc7c5eac4c198b869d9
Steve Miner <steveminer@gmail.com>
2024-01-06 15:49:30 -0500

    download

--- src/advent23/day08.clj
+++ src/advent23/day08.clj
@@ -112,6 +112,32 @@ XXX = (XXX, XXX)")
   (/ (abs (* a b))
      (gcd a b)))
 
+(require '[clojure.java.io :as io])
+(require '[clj-http.client :as client]
+
+(defn download-data
+  "if file doesn't already exist, download the data and store in
+  data/day#.dat"
+  [year day]
+  (let [fname (format "day%02d.dat" day)
+        url (str "https://adventofcode.com/" year "/day/" day "/input")
+        cookie "session=53616c7465645f5fd459afd5fd38064616059a49ef7f3073b9880ffc77754861018cb4be3fc007e9161a5347572d3192f2de781aae394be78902a9d9634d5685"
+        outfile (format "data/%s" fname)
+        ]
+    (if (not (.exists (io/file outfile)))
+      (let [data (:body (client/get url {:headers {:Cookie cookie}}))]
+        (spit outfile data))
+      "File already exists"
+      )))
+    
+(defn load-data [year day]
+  (download-data year day)
+  (let [fname (format "day%02d.dat" day)]
+  (slurp (str "data/" fname))))
+
+;;; end of original util
+
+
 (defn part2 [data]
   (let [[path graph] (parse data )
         starts (filter #(str/ends-with? % "A") (keys graph))
