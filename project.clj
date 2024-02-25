1b19f1c9a5c3ca0f0cff2233aaf93f4fc5dd3a0d
Steve Miner <steveminer@gmail.com>
2024-02-25 13:28:45 -0500

    alpha8

--- project.clj
+++ project.clj
@@ -41,7 +41,7 @@
   :global-vars {*warn-on-reflection* true
                 *unchecked-math* :warn-on-boxed}
   :profiles {:dev  {:source-paths ["src" "dev"]
-                    :dependencies [[org.clojure/clojure "1.12.0-alpha7"]
+                    :dependencies [[org.clojure/clojure "1.12.0-alpha8"]
                                    ;; [org.clojure/core.async "1.5.640"]
                                    ;;[clj-ns-browser "1.3.0" ]
                                    [criterium "0.4.6"]]}
