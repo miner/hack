3b72c399b74be8acaee0602fa0c6678d4fdb0380
Steve Miner <steveminer@gmail.com>
2024-09-10 14:22:39 -0400

    nfl

---
+++ src/miner/nfl.clj
@@ -0,0 +1,56 @@
+(ns miner.nfl
+  (:require [clojure.data.csv :as csv])
+  (:require [clojure.java.io :as io]))
+
+;;; Note: winner score first.  Not home/away
+
+(defn winner-score [x]
+  (parse-long (nth x 2)))
+
+(defn loser-score [x]
+  (parse-long (nth x 3)))
+
+(def extract-score (juxt winner-score loser-score))
+  
+;;; NFL data source
+;; https://www.pro-football-reference.com/boxscores/game-scores.htm
+
+(def nfl-scores-file "nfl-scores.csv")
+;; this file is only most recent example of that score, not all games
+;; ordered by most frequent ("count") score
+
+(def nfl-missing-scores-file "nfl-missing-scores.csv")
+;; I assume there's some implied max score.  Apparently, 70.
+
+
+;;; doall to force while file is open
+;;; skip header line
+(def nfl-data (with-open [reader (io/reader (io/resource nfl-scores-file))]
+                (doall (rest (csv/read-csv reader)))))
+
+;; pd is the point difference, not useful
+;; count might be useful
+(def nfl-keys [:rk :score :ptsw :ptsl :pttot :pd :count :xxx :last-game])
+
+(def nfl-scores (into (sorted-set) (map extract-score) nfl-data))
+
+
+(def example-data
+  [["1" "20-17" "20" "17" "37" "3" "291" "all games"
+    "Miami Dolphins vs. Jacksonville Jaguars September 8 2024"]
+   ["2" "27-24" "27" "24" "51" "3" "234" "all games"
+    "Kansas City Chiefs vs. Buffalo Bills January 21 2024"]
+   ["1085" "38-12" "38" "12" "50" "26" "1" "all games"
+    "Washington Redskins vs. New York Giants October 11 1987"]])
+
+
+
+  
+(def nfl-missing-data (with-open [reader (io/reader (io/resource nfl-missing-scores-file))]
+                        (doall (rest (csv/read-csv reader)))))
+
+(def nfl-missing-scores (into (sorted-set) (map extract-score) nfl-missing-data))
+
+
+
+               
