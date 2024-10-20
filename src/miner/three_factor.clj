295dadd4d766627412d3d2ba14c54e48155b69ef
Steve Miner <steveminer@gmail.com>
2024-10-20 19:56:08 -0400

    fact3

--- src/miner/three_factor.clj
+++ src/miner/three_factor.clj
@@ -9,6 +9,7 @@
 
 ;;; I thought I should write such a program in Clojure
 
+;;; slow
 (defn three-factor [product]
     (for [a (range 1 (inc product))
           b (range (inc a) (inc product))
@@ -21,14 +22,10 @@
 
 
 
-;;; better idea: do prime factorization and then find subsets.  Don't forget to allow 1.
-;;; 108 = 2 × 2 × 3 × 3 × 3
-
-
 (defn zmod? [x n]
   (zero? (rem x n)))
 
-;;; works but slower
+;;; faster
 (defn factor3 [product]
   (for [a (range 1 (inc product))
         :when (zmod? product a)
@@ -41,12 +38,69 @@
 
 #_ (require '[clojure.math.combinatorics :as mc])
 
+;;; better idea from Norvig: do factorization and then find subsets.  Don't forget to allow 1.
+;;; Note: I'm just finding integer factors, not prime factorization (with duplicates).  Not
+;;; sure which way is better
+;;; 108 = 2 × 2 × 3 × 3 × 3
+
+;;; slightly faster
+;;; all factors, not just prime
+
 (defn factors [product]
-  (filter #(zmod? product %) (range 1 (inc product))))
+  (conj (into [1] (filter #(zmod? product %)) (range 2 product)) product))
+
+(defn factors2 [product]
+  (filterv #(zmod? product %) (range 1 (inc product))))
+
+
+
 
 ;;; nice but slower!
 (defn factn [n product]
   (for [fs (mc/combinations (factors product) n)
         :when (= (apply * fs) product)]
-    (vec fs)))
+    fs))
 
+;;; might be nicer to return vectors but doesn't really matter
+
+
+;;; depends on factors being ordered which they are by construction
+;;; new fastest -- faster than drop
+(defn fact3 [product]
+  (let [fs (factors product)]
+    (for [a fs
+          b fs :when (> b a)
+          c fs :when (> c b) :when (= product (* a b c))]
+      [a b c])))
+
+;;; slightly faster but a little strange and not same order of results
+;;; not enough faster to justify strangeness
+(defn fact33 [product]
+  (let [fv (factors product)]
+    (for [a (rseq fv)
+          b fv :while (< b a)
+          c fv :when (> c b) :when (< c a) :when (= product (* a b c))]
+      [b c a])))
+
+;;; slightly slower
+(defn fact32 [product]
+  (let [fs (factors product)]
+    (for [a fs
+          b (drop-while #(<= % a) fs)
+          c (drop-while #(<= % b) fs) :when (= product (* a b c))]
+      [a b c])))
+
+
+
+(defn sorted-factors [product]
+  (into (sorted-set 1 product)
+        (filter #(zmod? product %))
+        (range 2 product)))
+
+;; not faster
+(defn fact3z [product]
+  (let [fs (sorted-factors product)]
+    (for [a fs
+          b (subseq fs > a)
+          c (subseq fs > b) :when (= product (* a b c))]
+      [a b c])))
