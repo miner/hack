45e83b82ef63d8bda022701a47ee02ce409fe5ac
Steve Miner <steveminer@gmail.com>
2024-11-02 16:22:19 -0400

    pascal triangle with init

--- src/miner/pascal.clj
+++ src/miner/pascal.clj
@@ -35,3 +35,35 @@
 ;;; from wiki, calc nth row directly
 (defn pascal-row [n]
   (reductions (fn [p k] (quot (* p (- (inc n) k)) k)) 1 (range 1 (inc n))))
+
+
+
+
+;;; See also
+;;; https://gist.github.com/PEZ/9dcf23444c51883c4318d69efcd5e9f7
+;;; https://ask.clojure.org/index.php/13937/stack-overflow-with-lazy-seq
+
+;;; This version takes an initial row instead of assuming [1].
+
+;;; init should be a vector, the initial row
+(defn pasctri [init]
+  (iterate #(mapv +' (conj (seq %) 0) (conj % 0)) init))
+
+
+
+(defn test-pasctri [pascfn]
+  (assert (= (second (pascfn [2 3 2])) [2 5 5 2]))
+  (assert (= (take 5 (pascfn [1])) [[1] [1 1] [1 2 1] [1 3 3 1] [1 4 6 4 1]]))
+  (assert (= (take 2 (pascfn [3 1 2])) [[3 1 2] [3 4 3 2]]))
+  (assert (= (take 100 (pascfn [2 4 2])) (rest (take 101 (pascfn [2 2])))))
+  true)
+
+
+;;; I didn't like this proposal as it uses explicit recursion and calls partition.
+;;; But the JVM seems to do a good job on it so performance is pretty good.
+(defn bad-pascal
+  ([row] (lazy-seq (cons row (bad-pascal row :next))))
+  ([row _]
+   (lazy-seq
+    (let [next-row (map #(apply +' %) (partition 2 1 (concat '(0) row '(0))))]
+      (cons next-row (bad-pascal next-row :next))))))
