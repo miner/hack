d3319addfe6e926116dbbb9525d460135ad30666
Steve Miner <steveminer@gmail.com>
2025-01-25 09:32:12 -0500

    more primes but not faster

--- src/miner/primes.clj
+++ src/miner/primes.clj
@@ -10,7 +10,8 @@
 ;;; 01/22/25 10:29 by miner -- see also Hacker News discussion of interesting, palindromic
 ;;; or memorable primes.  Lots of good ones.  https://news.ycombinator.com/item?id=42748691
 
-(ns miner.primes)
+(ns miner.primes
+  (:require [clojure.math :as m]))
 
 ;;; just a few
 (def memorable-primes
@@ -49,10 +50,14 @@
     (concat factors-below-sqrt factors-above-sqrt)))
 
 ;;; faster with vectors, inlined factor test, eager with transducer
+;;; should be faster if you guarantee odd N and only check for odd factors
+;;; would be better to only check prime factors but we're not that smart
 (defn factorv [n]
-  (let [factors-below-sqrt (filterv #(zero? (rem n %)) (range 1 (inc (Math/sqrt n))))]
-    (into factors-below-sqrt (map #(/ n %)) factors-below-sqrt)))
-
+  (when (odd? n)
+    (let [factors-below-sqrt (filterv #(zero? (rem n %)) (range 3 (inc (Math/sqrt n)) 2))]
+      (into (into factors-below-sqrt [1 n])
+            (map #(/ n %))
+            factors-below-sqrt))))
 
 
 (def get-factors (memoize factors))
@@ -64,6 +69,21 @@
 (defn primef? [n]
   (or (= n 2) (= (inc n) (sum-factors n))))
 
+;;; You don't need to actually calculate all the factors, just check for odds above 2
+;;; much faster than primef? but not great, only need to check odd factors
+;;; but see prime? below for faster way
+(defn primef2? [n]
+  (if (even? n)
+    (= n 2)
+    (and (> n 2) (empty? (filterv #(zero? (rem n %)) (range 3 (inc (Math/sqrt n)) 2))))))
+
+;;; note that sqrt(3) + 1 < 3 so it correctly returns true for 3 but almost by accientd
+
+;lazy is better in general but slower on actual prime
+(defn primef3? [n]
+  (if (even? n)
+    (= n 2)
+    (and (> n 2) (not-any? #(zero? (rem n %)) (range 3 (inc (Math/sqrt n)) 2)))))
 
 ;; http://sprott.physics.wisc.edu/pickover/pc/1000000000000066600000000000001.html
 ;; In John Milton's Paradise Lost, Belphegor is one of the "Principalities of the Prime." In hell, Belphegor is the demon of inventiveness.
@@ -194,4 +214,73 @@
     (prime? n)))
 
                 
-  
+
+(def odd-prime-cachev
+  [3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107 109
+   113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197 199 211 223 227 229 233
+   239 241 251 257 263 269 271 277 281 283 293 307 311 313 317 331 337 347 349 353 359 367
+   373 379 383 389 397 401 409 419 421 431 433 439 443 449 457 461 463 467 479 487 491 499
+   503 509 521 523 541 547 557 563 569 571 577 587 593 599 601 607 613 617 619 631 641 643
+   647 653 659 661 673 677 683 691 701 709 719 727 733 739 743 751 757 761 769 773 787 797
+   809 811 821 823 827 829 839 853 857 859 863 877 881 883 887 907 911 919 929 937 941 947
+   953 967 971 977 983 991 997])
+
+(def odd-prime-cache-max (peek odd-prime-cachev))
+
+(def odd-prime-cache-max-sq (* odd-prime-cache-max odd-prime-cache-max))
+
+(def odd-prime-cache-set (set odd-prime-cachev))
+
+;;; trying to be clever, but it's still slower than the simple prime?
+;;; the type hints did help
+(defn cprime? [^long n]
+  (cond (even? n) (= n 2)
+        (< n 2) false
+        (<= n odd-prime-cache-max) (odd-prime-cache-set n)
+        :else (let [rt (long (m/ceil (m/sqrt n)))]
+                (reduce (fn [res ^long p]
+                          (cond (> p rt) (reduced true)
+                                (zero? (rem n p)) (reduced false)
+                                :else true))
+                        true
+                        (if (< rt odd-prime-cache-max)
+                          odd-prime-cachev
+                          (concat odd-prime-cachev
+                                  (range (+ odd-prime-cache-max 2) rt 2)))))))
+
+
+;;; slower with loop, slower than cprime?
+(defn lprime? [^long n]
+  (cond (even? n) (= n 2)
+        (< n 2) false
+        (<= n odd-prime-cache-max) (odd-prime-cache-set n)
+        :else (let [rt (long (m/ceil (m/sqrt n)))]
+                (loop [cs  (if (< rt odd-prime-cache-max)
+                             odd-prime-cachev
+                             (concat odd-prime-cachev
+                                     (range (+ odd-prime-cache-max 2) rt 2)))]
+                  (let [p (long (first cs))]
+                    (cond (> p rt) true
+                          (zero? (rem n p)) false
+                          :else (recur (rest cs))))))))
+
+#_  (quick-bench (count (filterv cprime? (range 100000))))
+
+
+;;; still not faster, cache doesn't seem to help,  regular prime? is still faster on 10K
+;;; my guess is too many conditionals slows down the execution
+(defn iprime? [^long n]
+  (cond (even? n) (= n 2)
+        (< n 2) false
+        (<= n odd-prime-cache-max) (odd-prime-cache-set n)
+        :else (let [rt (long (m/ceil (m/sqrt n)))]
+                (loop [i 0]
+                  (if (< i (count odd-prime-cachev))
+                    (let [p (odd-prime-cachev i)]
+                      (cond (> p rt) true
+                            (zero? (rem n p)) false
+                            :else (recur (inc i))))
+                    (loop [p (+ odd-prime-cache-max 2)]
+                      (cond (> p rt) true
+                            (zero? (rem n p)) false
+                            :else (recur (inc (inc p))))))))))
