0aa0e55441ce9401719385e37ac365f20981b612
Steve Miner <steveminer@gmail.com>
2026-07-15 15:40:03 -0400

    alpha4

--- project.clj
+++ project.clj
@@ -49,8 +49,8 @@
                 ;; *unchecked-math* :warn-on-boxed
                 }
   :profiles {:dev  {:source-paths ["src" "dev"]
-                    :dependencies [[org.clojure/clojure "1.13.0-alpha3"]
-                                   [org.clojure/core.specs.alpha "0.6.92-alpha2"]
+                    :dependencies [[org.clojure/clojure "1.13.0-alpha4"]
+                                   [org.clojure/core.specs.alpha "0.6.113-alpha6"]
                                    [org.clojure/spec.alpha "0.6.249"]
                                    ;;[clj-ns-browser "1.3.0" ]
                                    [criterium "0.4.6"]]}
